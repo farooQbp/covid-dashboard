783c9f635ddaeb23c756ee57bb355a189e1f5c5f
Farooq Basheer <farooqbasheer000@gmail.com>
2024-10-09 04:23:08 +0530

    leaflet changes

--- src/components/mapview.tsx
+++ src/components/mapview.tsx
@@ -1,19 +1,27 @@
 import React from 'react';
 import { MapContainer, TileLayer, CircleMarker, Popup } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
+import { LatLngExpression } from 'leaflet'; // Import LatLngExpression type
 import { useSelector } from 'react-redux';
 import { RootState } from '../redux/store';
+import 'leaflet/dist/leaflet.css';
 
-const MapView = () => {
+const MapView: React.FC = () => {
   const { data } = useSelector((state: RootState) => state.covid);
 
+  // Define the center as LatLngExpression
+  const center: LatLngExpression = [20.5937, 78.9629]; // Coordinates for India
+
   return (
-    <MapContainer center={[20.5937, 78.9629]} zoom={5} style={{ height: '500px', width: '100%' }}>
+    <MapContainer
+      center={center} // Center prop must be typed as LatLngExpression
+      zoom={5}
+      style={{ height: '500px', width: '100%' }}
+    >
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
       {data.map((state) => (
         <CircleMarker
           key={state.name}
-          center={[state.name === 'Maharashtra' ? 19.7515 : 15.3173, state.name === 'Maharashtra' ? 75.7139 : 75.7139]} // Example coords
+          center={state.name === 'Maharashtra' ? [19.7515, 75.7139] : [15.3173, 75.7139]} // Example coords
           radius={Math.sqrt(state.totalCases) * 0.1}
           color="red"
         >
